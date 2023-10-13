9a11b0eae3689ccc8c31c16ae648dadb8ed776fc
cihuyyama <miqbalalhabib@gmail.com>
2023-10-13 22:20:40 +0700

    trying to fix pino-pretty bug

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -11,7 +11,7 @@ const log = logger({
     base: {
         pid: false
     },
-    timestamp: () => `,"time": "${dayjs().format()}"`
+    timestamp: () => `,"time": "${dayjs().format()}"`,
 })
 
 export default log
